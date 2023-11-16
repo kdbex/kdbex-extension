dda473db65fd63e02c92379be68e6d01f7528905
cuber <t.vinet@yahoo.fr>
2023-11-16 18:42:23 +0100

    init

---
+++ src/router/routes.ts
@@ -0,0 +1,21 @@
+import { RouteRecordRaw } from 'vue-router';
+
+const routes: RouteRecordRaw[] = [
+  {
+    path: '/',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
+      { path: 'popup', component: () => import('pages/PopupPage.vue') },
+      { path: '', component: () => import('pages/OptionsPage.vue') },
+    ],
+  },
+
+  // Always leave this as last one,
+  // but you can also remove it
+  {
+    path: '/:catchAll(.*)*',
+    component: () => import('pages/ErrorNotFound.vue'),
+  },
+];
+
+export default routes;
